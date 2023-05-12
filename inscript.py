8fcdadbec8f3e70cf106aff255d248e2b8978221
Mateus Miranda Torres <mateusmmt00@gmail.com>
2023-05-12 16:08:23 -0300

    a estrela

---
+++ inscript.py
@@ -0,0 +1,100 @@
+import math
+import json
+
+# LENDO ESTRUTURA DE DADOS E CONVERTENDO EM DICIONARIO
+with open("newMap.json", encoding='utf-8') as meu_json:
+	date_map = json.load(meu_json)
+
+def calcular_distancia(lat1, lon1, lat2, lon2):
+    R = 6371  # RAIO DA TERRA
+
+    # GRAUS P/ RADIANOS
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    # DIFERENÇA ENTRE AS COORDENADAS
+    delta_lat = lat2_rad - lat1_rad
+    delta_lon = lon2_rad - lon1_rad
+
+    # CALCULANDO A DISTANCIA
+    a = math.sin(delta_lat/2)**2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(delta_lon/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    distance = R * c * 1000
+
+    return round(distance, 2)
+
+
+def a_estrela(pontos, origem, destino):
+    # Inicialização
+    g = {}  # custo atual do ponto de origem até cada ponto
+    h = {}  # estimativa de custo do ponto até o destino
+    f = {}  # custo total (g + h) do ponto
+    pais = {}  # armazena o pai de cada ponto
+    abertos = []  # lista de pontos abertos para explorar
+
+    # Inicializa os custos com infinito, exceto para a origem
+    for ponto in pontos:
+        g[ponto['ponto']] = float('inf')
+        h[ponto['ponto']] = calcular_distancia(ponto['lat'], ponto['lon'], pontos[destino - 1]['lat'], pontos[destino - 1]['lon'])
+        f[ponto['ponto']] = float('inf')
+    g[origem] = 0
+    f[origem] = h[origem]
+    abertos.append(origem)
+
+    while abertos:
+        # Seleciona o ponto com o menor custo total (f)
+        atual = min(abertos, key=lambda ponto: f[ponto])
+
+        if atual == destino:
+            # Chegou ao ponto de destino
+            caminho = []
+            while atual in pais:
+                caminho.insert(0, atual)
+                atual = pais[atual]
+            caminho.insert(0, origem)
+            return caminho
+
+        abertos.remove(atual)
+
+        for vizinho_str in pontos[atual - 1]['pontos_alcancaveis'].split(';'):
+            if not vizinho_str:
+                continue
+
+            vizinho = int(vizinho_str)
+
+            # Calcula o custo atualizado para o vizinho
+            custo_atualizado = g[atual] + calcular_distancia(
+                pontos[atual - 1]['lat'], pontos[atual - 1]['lon'],
+                pontos[vizinho - 1]['lat'], pontos[vizinho - 1]['lon']
+            )
+
+            if custo_atualizado < g[vizinho]:
+                # Atualiza os custos e o pai do vizinho
+                pais[vizinho] = atual
+                g[vizinho] = custo_atualizado
+                f[vizinho] = g[vizinho] + h[vizinho]
+
+                if vizinho not in abertos:
+                    # Adiciona o vizinho à lista de pontos abertos
+                    abertos.append(vizinho)
+
+    # Não foi possível encontrar um caminho
+    return None
+
+# Exemplo de uso
+
+# print('\n1-Drogaria União \n2-Esquina Igreja \n3-Granja Leão \n4-GigaBites \n5-Drogaria PagMenos\n6-Paraiba\n7-Mercado Central\n8-Drogaria Globo\n9-Prime  Cell\n10-Subway\n11-Papelaria Globo\n12-Pub Pride\n13-Galeria dos Calçados\n14-Nortista\n15-Clinicou\n16-Impacto Kids\n17-Sobral\n18-Company Odonto\n19-DG Embalagens\n20-Armazem Zé Mendes\n21-Droga Vida')
+# origem = int(input('Escolha o ponto de origin: '))
+# destino = int(input('Escolha o ponto de origin: '))
+
+origem = 12
+destino = 6
+
+caminho = a_estrela(date_map, origem, destino)
+
+if caminho:
+    print(f"Caminho mais curto de {origem} a {destino}: {caminho}")
+else:
+    print(f"Não foi possível encontrar um caminho de {origem} a {destino}")
--- script.py
+++ script.py
@@ -1,74 +1,30 @@
 from numpy import sin, cos, arccos, pi, round
+import math
+import json
 # import pandas as pd
 
-map = [
-    [1, 'Drogaria União', -6.76718620245407, -43.0215998069604, [2, 11]],
-    [2, 'Esquina igreja', -6.76793880641175, -43.0215709496876, [1, 3, 12]],
-    [3, 'Granja Leão', -6.76885845672127, -43.0214988132572, [2, 4]],
-    [4, 'GigaBites', -6.76878643276312, -43.0204944028716, [3, 4, 12]],
-    [5, 'Drogaria PagMenos', -6.76872141035052, -43.0196630496312, [4, 6, 13]],
-    [6, 'Paraíba', -6.76868678971887, -43.0189560159711, [5, 7, 15]],
-    [7, 'Mercado Central', -6.76857119285080, -43.0175800629520, [6, 8, 21]],
-    [8, 'Drogaria Globo', -6.76797585186948, -43.0183128683907,	[9, 7, 19]],
-    [9, 'Prime Cell', -6.76771042412166, -43.0185437798245,	[8, 15, 10, 18]],
-    [10, 'Subway', -6.76695045067788, -43.019877359859,	[9, 11, 14, 16]],
-    [11,' Papelaria Globo', -6.76705748127151, -43.020605549343, [1, 10, 12]],
-    [12, 'Pub Pride', -6.76788306216676, -43.0205507817435,[2, 4, 11, 13]],
-    [13, 'Galeria dos Calçados', -6.76781873207370,	-43.019768465694, [5, 12, 14]],
-    [14, 'Nortista', -6.76764311827105,	-43.019781435176, [10, 13, 15]],
-    [15, 'Clinicou', -6.76799199336188,	-43.019010568988, [6, 9, 14]],
-    [16, 'Impacto Kids', -6.7663267823643, -43.0199349705603, [10, 17]],
-    [17, 'Sobral', -6.76620190217362, -43.0185431941594, [16, 18]],
-    [18, 'Company Odonto', -6.76716554683158, -43.0179304498840, [9, 17, 19]],
-    [19, 'DG Embalagens', -6.76760120229613, -43.0174994812226, [8, 18, 20]],
-    [20, 'Armazém Zé Mendes', -6.76828897917540, -43.0166349846579, [19, 21]],
-    [21, 'Droga Vida', -6.76893716772531, -43.0170946317711, [7, 20]],
-]
+# LENDO ESTRUTURA DE DADOS E CONVERTENDO EM DICIONARIO
+with open("map.json", encoding='utf-8') as meu_json:
+	date_map = json.load(meu_json)
+        
+print(date_map)
+print(date_map[1]['lat'], date_map[3]['lat'])
 
-new_map = []
+def distancia(lat1, lon1, lat2, lon2):
+    r = 6371000  # raio da Terra em metros
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    return r * c
 
 
-def rad2deg(radians):
-    degrees = radians * 180 / pi
-    return degrees
 
-def deg2rad(degrees):
-    radians = degrees * pi / 180
-    return radians
 
-def getDistEntPon(latitude1, longitude1, latitude2, longitude2, unit = 'kilometers'):
+for i in date_map:
+    pontos_alcancaveis = list(map(int, i['pontos_alcancaveis'].split(';')))
+
     
-    theta = longitude1 - longitude2
-    
-    distance = 60 * 1.1515 * rad2deg(
-        arccos(
-            (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) + 
-            (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta)))
-        )
-    )
-    
-    if unit == 'miles':
-        return round(distance, 2)
-    if unit == 'kilometers':
-        return round(distance * 1.609344, 2)
+    # print(f"Ponto {i['ponto']} alcança os pontos: {pontos_alcancaveis}")///
 
-def loc_id(lista, id):
-    for i in range(len(lista)):
-        if id == lista[i][0]:
-            return lista[i]
 
-for i in range(len(map)):
-    dist=[]
-    for j in range(len(map[i][4])):
-        id = map[i][4][j]
-        dist.append( [map [map [i] [4] [j]-1] [1] ,getDistEntPon(map[i][2], map[i][3], loc_id(map,map[i][4][j])[2], loc_id(map,map[i][4][j])[3])*1000] )
-        # print(dist)
-    
-    new_map.append([map[i][0],map[i][1],map[i][2],map[i][3],map[i][4], dist])
-    
-
-# print(loc_id(new_map, 8))
-# print(map[20])
-
-print(getDistEntPon(map[6][2], map[6][3], map[20][2], map[20][3]) * 10001)
-print(new_map)
